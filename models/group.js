d2b0c4db1455d1c072ed057214d68ee00fcfd0b9
Yehonatan Levin <jlevinana@gmail.com>
2026-07-26 23:51:17 +0300

    add group model with manager and membership methods

---
+++ models/group.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'שם הקבוצה הוא שדה חובה'],
+        trim: true,
+        minlength: [2, 'שם הקבוצה קצר מדי'],
+        maxlength: [50, 'שם הקבוצה ארוך מדי']
+    },
+    description: {
+        type: String,
+        trim: true,
+        maxlength: [500, 'התיאור לא יכול להכיל יותר מ-500 תווים'],
+        default: ''
+    },
+    city: {
+        type: String,
+        trim: true,
+        maxlength: [40, 'שם העיר ארוך מדי'],
+        default: ''
+    },
+    coverImage: {
+        type: String,
+        default: '/images/default-group.png'
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    timestamps: true
+});
+groupSchema.methods.isManager = function (userId) {
+    return this.creator.equals(userId);
+};
+
+groupSchema.methods.hasMember = function (userId) {
+    return this.members.some(m => m.equals(userId));
+};
+
+module.exports = mongoose.model('Group', groupSchema);
