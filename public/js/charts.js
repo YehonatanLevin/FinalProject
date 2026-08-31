7ea32a8d77d6b4b401622b634e03743ec8c775e2
YehonatanLevin <Jlevinana@gmail.com>
2026-08-31 16:04:14 +0300

    wrap long group labels onto two lines

--- public/js/charts.js
+++ public/js/charts.js
@@ -11,7 +11,7 @@
 
     /* גרף א' - עמודות: ק"מ לכל קבוצה בחודש הנוכחי */
     function barChart(rows) {
-        var margin = { top: 16, right: 16, bottom: 46, left: 52 };
+        var margin = { top: 16, right: 16, bottom: 58, left: 52 };
         var box = size('#groupKmChart');
         var width = box.width - margin.left - margin.right;
         var height = box.height - margin.top - margin.bottom;
@@ -40,10 +40,36 @@
          */
         var maxChars = Math.max(6, Math.floor((x.step() - 8) / 5.6));
 
-        function shorten(text) {
-            return text.length > maxChars
-                ? text.slice(0, maxChars - 1) + '\u2026'
-                : text;
+        /* שובר את שם הקבוצה לשתי שורות לפי מילים, כדי שלא ייחתך */
+        function wrapLabel(text) {
+            if (text.length <= maxChars) {
+                return [text];
+            }
+
+            var words = text.split(' ');
+            var lines = [];
+            var current = '';
+
+            for (var i = 0; i < words.length; i++) {
+                var candidate = current ? current + ' ' + words[i] : words[i];
+                if (candidate.length <= maxChars || current === '') {
+                    current = candidate;
+                } else {
+                    lines.push(current);
+                    current = words[i];
+                }
+            }
+            lines.push(current);
+
+            if (lines.length > 2) {
+                lines = [lines[0], lines.slice(1).join(' ')];
+            }
+
+            return lines.map(function (line) {
+                return line.length > maxChars
+                    ? line.slice(0, maxChars - 1) + '\u2026'
+                    : line;
+            });
         }
 
         svg.append('g').attr('transform', 'translate(0,' + height + ')')
@@ -52,9 +78,18 @@
             .style('text-anchor', 'middle')
             .style('fill', INK_SOFT)
             .style('font-size', '11px')
+            .attr('dy', null)
             .each(function (d) {
                 var node = d3.select(this);
-                node.text(shorten(d));
+                node.text(null);
+
+                wrapLabel(d).forEach(function (line, i) {
+                    node.append('tspan')
+                        .attr('x', 0)
+                        .attr('dy', i === 0 ? '0.71em' : '1.15em')
+                        .text(line);
+                });
+
                 node.append('title').text(d);
             });
 
